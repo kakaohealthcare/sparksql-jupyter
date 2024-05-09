44860479fabb5e112425bfb9da0efc4e0a0e11d3
grey parrot <grey.parrot@kakaohealthcare.com>
2024-05-09 10:59:29 +0900

    chore: pep8 format

--- sparksql_jupyter/sparksql.py
+++ sparksql_jupyter/sparksql.py
@@ -12,18 +12,28 @@ from jinja2 import Environment, StrictUndefined
 
 @magics_class
 class SparkSql(Magics):
-    limit = Int(20, config=True, help='The maximum number of rows to display')
-    
+    limit = Int(20, config=True, help="The maximum number of rows to display")
+
     @needs_local_scope
     @cell_magic
     @magic_arguments()
-    @argument('variable', nargs='?', type=str, help='Capture dataframe in a local variable')
-    @argument('-j', '--jinja', nargs='?', type=str, help='Capture dataframe in a local variable with jinja2 template')
-    @argument('-c', '--cache', action='store_true', help='Cache dataframe')
-    @argument('-e', '--eager', action='store_true', help='Cache dataframe with eager load')
-    @argument('-v', '--view', type=str, help='Create or replace temporary view')
-    @argument('-l', '--limit', type=int, help='The maximum number of rows to display')
-    def sparksql(self, line='', cell='', local_ns=None):
+    @argument(
+        "variable", nargs="?", type=str, help="Capture dataframe in a local variable"
+    )
+    @argument(
+        "-j",
+        "--jinja",
+        nargs="?",
+        type=str,
+        help="Capture dataframe in a local variable with jinja2 template",
+    )
+    @argument("-c", "--cache", action="store_true", help="Cache dataframe")
+    @argument(
+        "-e", "--eager", action="store_true", help="Cache dataframe with eager load"
+    )
+    @argument("-v", "--view", type=str, help="Create or replace temporary view")
+    @argument("-l", "--limit", type=int, help="The maximum number of rows to display")
+    def sparksql(self, line="", cell="", local_ns=None):
         if local_ns is None:
             local_ns = {}
 
@@ -40,29 +50,41 @@ class SparkSql(Magics):
 
         df = spark.sql(bind_variables(cell, user_ns))
         if args.cache or args.eager:
-            print('cache dataframe with %s load' % ('eager' if args.eager else 'lazy'))
+            print("cache dataframe with %s load" % ("eager" if args.eager else "lazy"))
             df = df.cache()
             if args.eager:
                 df.count()
         if args.view:
-            print('create temporary view `%s`' % args.view)
+            print("create temporary view `%s`" % args.view)
             df.createOrReplaceTempView(args.view)
         if args.variable:
-            print('capture dataframe to local variable from jinja template`%s`' % args.variable)
+            print(
+                "capture dataframe to local variable from jinja template`%s`"
+                % args.variable
+            )
             self.shell.user_ns.update({args.variable: df})
 
         limit = args.limit or self.limit
         header, contents = get_results(df, limit)
         if len(contents) > limit:
-            print('only showing top %d row(s)' % limit)
+            print("only showing top %d row(s)" % limit)
 
-        html = make_tag('tr',
-                        ''.join(map(lambda x: make_tag('td', escape(x), style='font-weight: bold'), header)),
-                        style='border-bottom: 1px solid')
+        html = make_tag(
+            "tr",
+            "".join(
+                map(
+                    lambda x: make_tag("td", escape(x), style="font-weight: bold"),
+                    header,
+                )
+            ),
+            style="border-bottom: 1px solid",
+        )
         for index, row in enumerate(contents[:limit]):
-            html += make_tag('tr', ''.join(map(lambda x: make_tag('td', escape(x)), row)))
+            html += make_tag(
+                "tr", "".join(map(lambda x: make_tag("td", escape(x)), row))
+            )
 
-        return HTML(make_tag('table', html))
+        return HTML(make_tag("table", html))
 
 
 def bind_variables(query, user_ns):
@@ -73,25 +95,27 @@ def bind_variables(query, user_ns):
 def get_results(df, limit):
     def convert_value(value):
         if value is None:
-            return 'null'
+            return "null"
         return str(value)
 
     header = df.columns
     for column_name, data_type in df.dtypes:
         if data_type == "timestamp":
-            df = df.withColumn(column_name, date_format(df[column_name], "yyyy-MM-dd HH:mm:ss"))
-        
+            df = df.withColumn(
+                column_name, date_format(df[column_name], "yyyy-MM-dd HH:mm:ss")
+            )
+
     contents = list(map(lambda row: list(map(convert_value, row)), df.take(limit + 1)))
 
     return header, contents
 
 
-def make_tag(tag_name, body='', **kwargs):
-    attributes = ' '.join(map(lambda x: '%s="%s"' % x, kwargs.items()))
+def make_tag(tag_name, body="", **kwargs):
+    attributes = " ".join(map(lambda x: '%s="%s"' % x, kwargs.items()))
     if attributes:
-        return '<%s %s>%s</%s>' % (tag_name, attributes, body, tag_name)
+        return "<%s %s>%s</%s>" % (tag_name, attributes, body, tag_name)
     else:
-        return '<%s>%s</%s>' % (tag_name, body, tag_name)
+        return "<%s>%s</%s>" % (tag_name, body, tag_name)
 
 
 def get_instantiated_spark_session():
--- tests/test_sparksql.py
+++ tests/test_sparksql.py
@@ -4,52 +4,71 @@ from pyspark import Row
 from unittest import mock
 from sparksql_jupyter.sparksql import bind_variables, get_results, make_tag
 from jinja2 import exceptions
-import pandas as pd
+
 
 @pytest.fixture
 def df():
     return mock.MagicMock(
-        columns=['col1', 'col2', 'col3'],
-        take=lambda n: [Row(col1=i, col2=str(i), col3=None) for i in range(n)]
+        columns=["col1", "col2", "col3"],
+        take=lambda n: [Row(col1=i, col2=str(i), col3=None) for i in range(n)],
     )
 
 
 @pytest.fixture
 def user_ns():
     return {
-        'month': 5,
-        'day': '10',
-        'schema': 'dl',
-        'start_var': "'2019-05-01'",
-        'end_var': "'2019-05-31'"
+        "month": 5,
+        "day": "10",
+        "schema": "dl",
+        "start_var": "'2019-05-01'",
+        "end_var": "'2019-05-31'",
     }
+
 
 @pytest.fixture
 def df_ts():
     return mock.MagicMock(
-        columns=['col1'],
-        take=lambda n: [Row(col1=datetime.datetime(1, 1, i, 0, 0, 0)) for i in range(1, n)]
+        columns=["col1"],
+        take=lambda n: [
+            Row(col1=datetime.datetime(1, 1, i, 0, 0, 0)) for i in range(1, n)
+        ],
     )
 
+
 def test_get_results(df):
-    assert get_results(df, 0) == (['col1', 'col2', 'col3'], [['0', '0', 'null']])
-    assert get_results(df, 1) == (['col1', 'col2', 'col3'], [['0', '0', 'null'], ['1', '1', 'null']])
+    assert get_results(df, 0) == (["col1", "col2", "col3"], [["0", "0", "null"]])
+    assert get_results(df, 1) == (
+        ["col1", "col2", "col3"],
+        [["0", "0", "null"], ["1", "1", "null"]],
+    )
 
 
 def test_bind_variables(user_ns):
-    assert bind_variables('SELECT * FROM table', user_ns) == 'SELECT * FROM table'
-    assert bind_variables("SELECT * FROM {{schema}}.table WHERE lsh_dtm between {{start_var}} and {{end_var}}", user_ns) == "SELECT * FROM dl.table WHERE lsh_dtm between '2019-05-01' and '2019-05-31'"
+    assert bind_variables("SELECT * FROM table", user_ns) == "SELECT * FROM table"
+    assert (
+        bind_variables(
+            "SELECT * FROM {{schema}}.table WHERE lsh_dtm between {{start_var}} and {{end_var}}",
+            user_ns,
+        )
+        == "SELECT * FROM dl.table WHERE lsh_dtm between '2019-05-01' and '2019-05-31'"
+    )
 
     with pytest.raises(exceptions.UndefinedError):
         bind_variables("SELECT * FROM table WHERE hour='{{hour}}'", user_ns)
 
 
 def test_make_tag():
-    assert make_tag('td') == '<td></td>'
-    assert make_tag('td', 'body') == '<td>body</td>'
-    assert make_tag('td', 'body', style='font-weight: bold') == '<td style="font-weight: bold">body</td>'
+    assert make_tag("td") == "<td></td>"
+    assert make_tag("td", "body") == "<td>body</td>"
+    assert (
+        make_tag("td", "body", style="font-weight: bold")
+        == '<td style="font-weight: bold">body</td>'
+    )
 
 
 def test_timestamp_with_timezone(df_ts):
-    assert get_results(df_ts, 1) == (['col1'], [['0001-01-01 00:00:00']])
-    assert get_results(df_ts, 2) == (['col1'], [['0001-01-01 00:00:00'], ['0001-01-02 00:00:00']])
+    assert get_results(df_ts, 1) == (["col1"], [["0001-01-01 00:00:00"]])
+    assert get_results(df_ts, 2) == (
+        ["col1"],
+        [["0001-01-01 00:00:00"], ["0001-01-02 00:00:00"]],
+    )
